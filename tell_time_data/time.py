6c19ad86c99b53ba014bd78985a4ad5fee1bfb66
lol2331 <114163487+lol2331@users.noreply.github.com>
2023-04-02 12:05:36 +0300

    Acum merge

--- tell_time_data/time.py
+++ tell_time_data/time.py
@@ -1,10 +1,14 @@
 
 from datetime import datetime
 import time
+import os
 
-for i in range(100):
+log_file_path = os.path.join(os.getcwd(), "log.txt")
+
+for i in range(4):
+    with open(log_file_path, "a") as log_file:
+        log_file.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n")
     time.sleep(5)
-    file = open("log.txt", "a")
-    time.sleep(1)
-    file.write(datetime.today().strftime('%Y-%m-%d %H:%M:%S')+"\n")
-file.close()
+
+log_file_path = os.path.join(os.getcwd(), "log.txt")
+print(log_file_path)
